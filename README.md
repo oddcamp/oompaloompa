e804db95ead0751ce9ac43a48953a0e51ded71b2
Henrik Sjökvist <henrik.sjokvist@gmail.com>
2014-02-27 00:02:16 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oompa Loompa
+
+Ooompa Loopma is a simple web service that listens to POST hooks from Bitbucket or GitHub 
---
+++ oompaloompa.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func logHandler(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%v: %v %v", r.Method, r.URL.Path, r.PostFormValue("project"))
+		next.ServeHTTP(w, r)
+	}
+}
+
+func logHandlerFunc(next http.HandlerFunc) http.HandlerFunc {
+	return logHandler(next)
+}
+
+type Project struct {
+	Name, Path string
+}
+type Configuration struct {
+	Projects []Project
+}
+
+type Payload struct {
+	Repository struct {
+		Name string
+	}
+}
+
+var configuration = &Configuration{}
+var port = flag.Int("port", 3000, "listen on port")
+
+func main() {
+	log.Println("Oompa Loompa warming up...")
+	flag.Parse()
+	file, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(file)
+	decoder.Decode(&configuration)
+
+	http.Handle("/deploy", logHandlerFunc(deploy))
+	http.Handle("/", logHandlerFunc(home))
+
+	portString := strconv.Itoa(*port)
+	log.Println("Listening on port " + portString + "...")
+	http.ListenAndServe(":"+portString, nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Oompa Loompa reporting for duty!"))
+}
+
+func projectIndex(t string, vs []Project) int {
+	for i, v := range vs {
+		if v.Name == t {
+			return i
+		}
+	}
+	return -1
+}
+
+func deploy(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		log.Println("Payload received.")
+
+		payload := &Payload{}
+		var decoder = json.NewDecoder(strings.NewReader(r.PostFormValue("payload")))
+		decoder.Decode(&payload)
+
+		index := projectIndex(payload.Repository.Name, configuration.Projects)
+		if index < 0 {
+			log.Println("Invalid payload.")
+			return
+		}
+
+		log.Println("Deploying '" + configuration.Projects[index].Name + "'...")
+		err := os.Chdir(configuration.Projects[index].Path)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		out, err := exec.Command("git", "pull").Output()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		log.Println(string(out[:]))
+	}
+}
